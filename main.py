9426ae93a1e7b20115acd2d701f38895bc0efbf7
Aabhas Kumar Jha <abhasjha03@gmail.com>
2021-10-29 10:45:46 +0530

    added bootstrap

--- main.py
+++ main.py
@@ -13,6 +13,10 @@ def create_app(testing = False):
     @app.route("/")
     def index():
         return render_template("index.html")
+
+    @app.route("/about")
+    def about():
+        return render_template("about.html")
     
     return app
 

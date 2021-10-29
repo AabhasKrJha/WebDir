8701e4bf8a4d2dd5a8336fd6ddcb7843ce040079
Aabhas Kumar Jha <abhasjha03@gmail.com>
2021-10-29 20:18:11 +0530

    added dark-mode

---
+++ README.md
@@ -0,0 +1,3 @@
+# Flask Dir
+
+Templates for drontend and backend components and code snippets for flask/python and bootstrap in the form of a webapp, made using flask and bootstrap itself.
--- static/js/app.js
+++ static/js/app.js
@@ -1,1 +1,43 @@
-console.log("Hello, world!")
+function switchTheme(){
+
+    let btn = document.getElementById('theme-btn');
+    let logo = document.getElementById('flask-logo');
+    let body = document.body.style;
+    let nav_links = document.getElementsByClassName('navbar-link');
+
+    if (btn.classList.contains("btn-dark")){
+
+        // dark mode properties
+
+        btn.classList.remove('btn-dark');
+        btn.classList.add('btn-light');
+
+        body.backgroundColor = "black";
+        body.color = 'white';
+
+        for (var i = 0; i < nav_links.length; i++){
+            nav_links[i].style.color = 'white';
+        };
+
+        logo.src = "/static/media/img/flask-logo-dark.png";
+
+    }else{
+
+        //light mode properties
+
+        btn.classList.remove('btn-light');
+        btn.classList.add('btn-dark');
+
+        body.backgroundColor = "white";
+        body.color = 'black';
+
+        for (var i = 0; i < nav_links.length; i++){
+            nav_links[i].style.color = 'black';
+        };
+
+        logo.src = "/static/media/img/flask-logo.png";
+
+    }
+
+
+}
